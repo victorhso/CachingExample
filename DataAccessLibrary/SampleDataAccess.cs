98a3597696c8ce42b247d1b82b1d04004f7c3e6e
Victor Oliveira <victor.souza.41@hotmail.com>
2023-07-12 21:43:18 -0300

    Add project files.

---
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccessLibrary
+{
+    public class SampleDataAccess
+    {
+        private readonly IMemoryCache _memoryCache;
+        public SampleDataAccess(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> output = new();
+
+            output.Add(new() { FirstName = "Victor", LastName = "Oliveira", });
+            output.Add(new() { FirstName = "Lucas", LastName = "Alves", });
+            output.Add(new() { FirstName = "Fabio", LastName = "Tirela", });
+
+            Thread.Sleep(3000);
+
+            return output;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesAsync()
+        {
+            List<EmployeeModel> output = new();
+
+            output.Add(new() { FirstName = "Victor", LastName = "Oliveira", });
+            output.Add(new() { FirstName = "Lucas", LastName = "Alves", });
+            output.Add(new() { FirstName = "Fabio", LastName = "Tirela", });
+
+            await Task.Delay(3000);
+
+            return output;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesCache()
+        {
+            List<EmployeeModel> output;
+
+            output = _memoryCache.Get<List<EmployeeModel>>("employees");
+
+            if (output is null)
+            {
+                output = new();
+
+                output.Add(new() { FirstName = "Victor", LastName = "Oliveira", });
+                output.Add(new() { FirstName = "Vinicius", LastName = "Moreira", });
+                output.Add(new() { FirstName = "Fabio", LastName = "Tirela", });
+
+                await Task.Delay(3000);
+
+                _memoryCache.Set("employees", output, TimeSpan.FromSeconds(7));
+            }
+
+            return output;
+        }
+    }
+}
